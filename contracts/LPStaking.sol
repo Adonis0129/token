8036eea58f75ee40a3a0f95b73b8b8e7e0b3cd69
Adonis <goldendores9826@gmail.com>
2022-08-14 00:28:35 +0200

    buy LP with any token

--- contracts/LPStaking.sol
+++ contracts/LPStaking.sol
@@ -7,15 +7,29 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
-contract LPStaking is BaseContract, ERC20Upgradeable {
+/**
+ * @title Furio AddLiquidity
+ * @author Steve Harmeyer
+ * @notice This contract offers LP holders can stake their holded LP token.
+ */
+
+/// @custom:security-contact security@furio.io
+contract LPStaking is BaseContract, ERC20Upgradeable 
+{
     using SafeMath for uint256;
 
+    /**
+     * Contract initializer.
+     * @dev This intializes all the parent contracts.
+     */
     function initialize() public initializer {
         __BaseContract_init();
         _lastUpdateTime = block.timestamp;
         _dividendsPerShareAccuracyFactor = 1e36;
     }
-
+    /**
+     * Staker struct.
+     */
     struct Staker {
         uint256 stakingAmount;
         uint256 boostedAmount;
@@ -23,29 +37,41 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         uint256 lastStakingUpdateTime;
         uint256 stakingPeriod;
     }
-
+    /**
+     * variables
+     */
     address public lpAddress;
-    uint256  _lastUpdateTime;
-    uint256  _accLPPerShare;
-    uint256  _dividendsPerShareAccuracyFactor;
+    uint256  _lastUpdateTime; //LP RewardPool Updated time
+    uint256  _accLPPerShare;  //Accumulated LPs per share, times 1e36. See below.
+    uint256  _dividendsPerShareAccuracyFactor; //1e36
 
-    uint256 public stakerNum;
-    uint256 public totalStakingAmount;
-    uint256  _totalBoostedAmount;
-    uint256  _totalReward;
-    uint256  _totalReflection; 
-    uint256  _LPLockAmount;
-    address  _LPLockReceiver;
-    address[] LPholders;
+    uint256 public totalStakerNum; //total staker number
+    uint256 public totalStakingAmount; //total staker amount
+    uint256  _totalBoostedAmount; //total boosted amount for reward distrubution
+    uint256  _totalReward;  //total LP amount for LP reward to LP stakers
+    uint256  _totalReflection; //total LP amount to LP reflection to LP holders
+    uint256  _LPLockAmount; // total locked LP amount. except from LP reflection
+    address  _LPLockReceiver; //address for LP lock
+    address[] LPholders; // LP holders address. to get LP reflection, they have to register thier address here.
 
+    /**
+     * Mappings.
+     */
     mapping(address => Staker) public stakers;
     mapping(address => uint256) _LPholderIndexes;
 
+    /**
+     * Event.
+     */
     event Stake(address indexed staker, uint256 amount, uint256 duration);
     event ClaimRewards(address indexed staker, uint256 amount);
     event Compound(address indexed staker, uint256 amount);
     event Unstake(address indexed staker, uint256 amount);
 
+    /**
+     * Update addresses.
+     * @dev Updates stored addresses.
+     */
     function updateAddresses() public {
         IUniswapV2Factory _factory_ = IUniswapV2Factory(
             addressBook.get("factory")
@@ -57,10 +83,18 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         _LPLockReceiver = addressBook.get("lpLockReceiver");
     }
 
+    /**
+     * total LP amount holed this contract
+     */
     function _LPSupply_() external view returns (uint256) {
         return IERC20(lpAddress).balanceOf(address(this));
     }
 
+    /**
+     * claimable Reward for LP stakers
+     * @param stakerAddress_ staker address
+     * @return pending_ claimable LP amount
+     */
     function pendingReward(address stakerAddress_)
         public
         view
@@ -74,6 +108,10 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
             .sub(stakers[stakerAddress_].rewardDebt);
     }
 
+    /**
+     * Update reward pool for LP stakers.
+     * @dev update _accLPPerShare
+     */
     function updateRewardPool() public {
         if (lpAddress == address(0)) updateAddresses();
 
@@ -101,6 +139,12 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         _lastUpdateTime = _lastUpdateTime.add(_times_.mul(24 hours));
     }
 
+    /**
+     * stake function
+     * @param amount_ LP amount
+     * @param durationIndex_ duration index.
+     * @dev approve LP before staking.
+     */
     function stake(uint256 amount_, uint256 durationIndex_) public {
         if (lpAddress == address(0) || _LPLockReceiver == address(0))
             updateAddresses();
@@ -108,7 +152,7 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         require(IERC20(lpAddress).balanceOf(msg.sender) >= amount_, "Insufficient balance!");
         require(durationIndex_ <= 3, "Non exist duration!");
 
-        if (stakers[msg.sender].stakingAmount == 0) stakerNum++;
+        if (stakers[msg.sender].stakingAmount == 0) totalStakerNum++;
 
         updateRewardPool();
 
@@ -173,6 +217,10 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         );
     }
 
+    /**
+     * claim reward function for LP stakers
+     @notice stakers can claim every 24 hours.
+     */
     function claimRewards() public {
         if (lpAddress == address(0)) updateAddresses();
 
@@ -192,6 +240,10 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         emit ClaimRewards(msg.sender, _pending_);
     }
 
+    /**
+     * compound function for LP stakers
+     @notice stakers restake claimable LP every 24 hours without staking fee.
+     */
     function compound() public {
         if (lpAddress == address(0)) updateAddresses();
         if (stakers[msg.sender].stakingAmount <= 0) return;
@@ -212,6 +264,10 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         emit Compound(msg.sender, _pending_);
     }
 
+    /**
+     * unstake function
+     @notice stakers have to claim rewards before finishing stake.
+     */
     function unstake() public {
         if (lpAddress == address(0) || _LPLockReceiver == address(0))
             updateAddresses();
@@ -236,7 +292,7 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         totalStakingAmount = totalStakingAmount.sub(
             stakers[msg.sender].stakingAmount
         );
-        stakerNum--;
+        totalStakerNum--;
 
         stakers[msg.sender].stakingAmount = 0;
         stakers[msg.sender].boostedAmount = 0;
@@ -247,6 +303,10 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         emit Unstake(msg.sender, _amount_);
     }
 
+    /**
+     * register LP holders address
+     @notice LP holders have to register their address to get LP reflection.
+     */
     function registerAddress() public {
         if (_LPLockReceiver == address(0)) updateAddresses();
         if (msg.sender == _LPLockReceiver) return;
@@ -254,12 +314,18 @@ contract LPStaking is BaseContract, ERC20Upgradeable {
         LPholders.push(msg.sender);
     }
 
+    /**
+     * remove LP holders address
+     */
     function removeShareholder(address _holder) public {
         LPholders[_LPholderIndexes[_holder]] = LPholders[LPholders.length - 1];
         _LPholderIndexes[LPholders[LPholders.length - 1]] = _LPholderIndexes[_holder];
         LPholders.pop();
     }
 
+    /**
+     * LP reflection whenever stake and unstake
+     */
     function _distributeReflectionRewards() internal {
         if (lpAddress == address(0)) updateAddresses();
 
--- contracts/LPSwap.sol
+++ contracts/LPSwap.sol
@@ -12,160 +12,270 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 /**
  * @title LP buy and sell
  * @author Steve Harmeyer
- * @notice This contrac make users can buy and sell LP with USDC.
+ * @notice This contrac make users can buy LP with any token and get USDC by selling LP.
  */
 
 /// @custom:security-contact security@furio.io
-contract LPSwap is BaseContract
-{
-
+contract LPSwap is BaseContract {
     // is necessary to receive unused bnb from the swaprouter
     receive() external payable {}
+
+    address _routerAddress;
+    address _tokenAddress;
+    address _usdcAddress;
+    address _lpAddress;
+    IUniswapV2Router02 public router;
+    mapping(address => address[]) public pathFromTokenToUSDC;
 
     /**
      * Contract initializer.
      * @dev This intializes all the parent contracts.
      */
-    function initialize() initializer public
-    {
+    function initialize() public initializer {
         __BaseContract_init();
     }
 
-    function buyLP(uint256 paymentAmount_) public returns(uint256 lpAmount_)
-    {
-        IERC20 _payment_ = IERC20(addressBook.get("payment"));
-        require(_payment_.balanceOf(msg.sender) >= paymentAmount_, "Invalid amount");
-        _payment_.transferFrom(msg.sender, address(this), paymentAmount_);
+    /**
+     * Set Swap router path to swap any token to USDC
+     * @param token_ token address to swap
+     * @param pathToUSDC_ path address array
+     */
+    function setSwapPathFromTokenToUSDC(
+        address token_,
+        address[] memory pathToUSDC_
+    ) public onlyOwner {
 
-        lpAmount_ = _buyLPwithUSDC(paymentAmount_);
-        return lpAmount_;
+        if (_usdcAddress == address(0)) updateAddresses();
+
+        require(token_ != address(0), "Invalid token address");
+        require(pathToUSDC_.length >= 2, "Invalid path length");
+        require(pathToUSDC_[0] == token_, "Invalid starting token");
+        require(pathToUSDC_[pathToUSDC_.length - 1] == _usdcAddress,"Invalid ending token");
+
+        pathFromTokenToUSDC[token_] = pathToUSDC_;
     }
 
-    function _buyLPwithUSDC(uint256 paymentAmount_) internal returns(uint256 amountLP_){
-        IERC20 _in_ = IERC20(addressBook.get("payment"));
-        require(address(_in_) != address(0), "Payment not set");
-        IERC20 _out_ = IERC20(addressBook.get("token"));
-        require(address(_out_) != address(0), "Token not set");
+    /**
+     * Update addresses.
+     * @dev Updates stored addresses.
+     */
+    function updateAddresses() public {
+
+        IUniswapV2Factory _factory_ = IUniswapV2Factory(addressBook.get("factory"));
+        _lpAddress = _factory_.getPair(
+            addressBook.get("payment"),
+            addressBook.get("token")
+        );
+        _routerAddress = addressBook.get("router");
+        _tokenAddress = addressBook.get("token");
+        _usdcAddress = addressBook.get("payment");
+    }
+
+    /**
+     * buy LP with any token
+     * @param paymentAddress_ token address that user is going to buy LP
+     * @param paymentAmount_ token amount that user is going to buy LP
+     * @return lpAmount_ LP amount that user received
+     * @return unusedPaymentToken_ USDC amount that don't used to buy LP
+     * @return unusedToken_ token amount that don't used to buy LP
+     * @dev approve token before buyLP
+     */
+    function buyLP(address paymentAddress_, uint256 paymentAmount_)
+        public
+        payable
+        returns (
+            uint256 lpAmount_,
+            uint256 unusedPaymentToken_,
+            uint256 unusedToken_
+        )
+    {
+
+        if (_routerAddress == address(0) || _usdcAddress == address(0)) updateAddresses();
+        router = IUniswapV2Router02(_routerAddress);
+        IERC20 _usdc_ = IERC20(_usdcAddress);
+        require(address(paymentAddress_) != address(0), "Invalid Address");
+        IERC20 _payment_ = IERC20(paymentAddress_);
+
+        if(paymentAddress_ == router.WETH()){
+            require(msg.value >= paymentAmount_, "Invalid amount");
+
+            address[] memory _pathFromEthToUSDC = pathFromTokenToUSDC[paymentAddress_];
+            uint256 _USDCBalanceBefore_ = _usdc_.balanceOf(address(this));
+            router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: paymentAmount_}(
+                0, 
+                _pathFromEthToUSDC, 
+                address(this), 
+                block.timestamp + 1
+            );
+            uint256 _USDCBalance_ = _usdc_.balanceOf(address(this)) - _USDCBalanceBefore_;
+
+            (lpAmount_, unusedPaymentToken_, unusedToken_) = _buyLPwithUSDC(_USDCBalance_);
+            return (lpAmount_, unusedPaymentToken_, unusedToken_);
+
+        }
+        else
+        {
+            require(_payment_.balanceOf(msg.sender) >= paymentAmount_,"Invalid amount");
+            _payment_.transferFrom(msg.sender, address(this), paymentAmount_);
+
+            if (paymentAddress_ == _usdcAddress) {
+
+                (lpAmount_, unusedPaymentToken_, unusedToken_) = _buyLPwithUSDC(paymentAmount_);
+                return (lpAmount_, unusedPaymentToken_, unusedToken_);
+            }
+            
+            address[] memory _pathFromTokenToUSDC = pathFromTokenToUSDC[paymentAddress_];
+            _payment_.approve(address(router), paymentAmount_);
+            uint256 _USDCBalanceBefore1_ = _usdc_.balanceOf(address(this));
+            router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+                paymentAmount_,
+                0, 
+                _pathFromTokenToUSDC, 
+                address(this), 
+                block.timestamp + 1
+            );
+            uint256 _USDCBalance1_ = _usdc_.balanceOf(address(this)) - _USDCBalanceBefore1_;
+
+            (lpAmount_, unusedPaymentToken_, unusedToken_) = _buyLPwithUSDC(_USDCBalance1_);
+            return (lpAmount_, unusedPaymentToken_, unusedToken_);
+        }
+    }
+
+    /**
+     * buy LP with USDC
+     * @param paymentAmount_ USDC amount that user is going to buy LP
+     * @return lpAmount_ LP amount that user received
+     * @return unusedPaymentToken_ USDC amount that don't used to buy LP
+     * @return unusedToken_ Token amount that don't used to buy LP
+     * @notice buyer can get unused USDC and token automatically
+     */
+    function _buyLPwithUSDC(uint256 paymentAmount_)
+        internal
+        returns (
+            uint256 lpAmount_,
+            uint256 unusedPaymentToken_,
+            uint256 unusedToken_
+        )
+    {
+        if (
+            _routerAddress == address(0) ||
+            _tokenAddress == address(0) ||
+            _usdcAddress == address(0)
+        ) updateAddresses();
+
+        IERC20 _usdc_ = IERC20(_usdcAddress);
+        IToken _token_ = IToken(_tokenAddress);
+        router = IUniswapV2Router02(_routerAddress);
+        require(address(_usdc_) != address(0), "Payment token not set");
+        require(address(_token_) != address(0), "Token not set");
+        require(address(router) != address(0), "Router not set");
 
         uint256 _amountToLiquify_ = paymentAmount_ / 2;
         uint256 _amountToSwap_ = paymentAmount_ - _amountToLiquify_;
-        if(_amountToSwap_ == 0) return 0;
-        uint256 _amountToken_ = _swap(_in_, _out_, _amountToSwap_);
-        amountLP_ = _addLiquidity(_amountToLiquify_, _amountToken_);
-        return amountLP_;
-    }
-
-    function _swap(IERC20 in_, IERC20 out_, uint256 amount_) internal returns (uint256 amountOut_)
-    {
-        IUniswapV2Router02 _router_ = IUniswapV2Router02(addressBook.get("router"));
-        require(address(_router_) != address(0), "Router not set");
-        if(amount_ <= 0) return 0;
+        if (_amountToSwap_ == 0) return (0, 0, 0);
 
         address[] memory _path_ = new address[](2);
-        _path_[0] = address(in_);
-        _path_[1] = address(out_);
-        in_.approve(address(_router_), amount_);
-        uint256 _balanceBefore_ = out_.balanceOf(address(this));
-
-        _router_.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-            amount_,
+        _path_[0] = address(_usdc_);
+        _path_[1] = address(_token_);
+        _usdc_.approve(address(router), _amountToSwap_);
+        uint256 _balanceBefore_ = _token_.balanceOf(address(this));
+        router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            _amountToSwap_,
             0,
             _path_,
             address(this),
-            block.timestamp
+            block.timestamp + 1
         );
-        amountOut_ = out_.balanceOf(address(this)) - _balanceBefore_;
-        return amountOut_;
+        uint256 _amountToken_ = _token_.balanceOf(address(this)) -_balanceBefore_;
+
+        if (_amountToLiquify_ <= 0 || _amountToken_ <= 0) return (0, 0, 0);
+        _usdc_.approve(address(router), _amountToLiquify_);
+        _token_.approve(address(router), _amountToken_);
+
+        (
+            uint256 _usedPaymentToken_,
+            uint256 _usedToken_,
+            uint256 _lpValue_
+        ) = router.addLiquidity(
+                address(_usdc_),
+                address(_token_),
+                _amountToLiquify_,
+                _amountToken_,
+                0,
+                0,
+                msg.sender,
+                block.timestamp + 1
+            );
+        lpAmount_ = _lpValue_;
+        unusedPaymentToken_ = _amountToLiquify_ - _usedPaymentToken_;
+        unusedToken_ = _amountToken_ - _usedToken_;
+
+        // send back unused tokens
+        _usdc_.transfer(msg.sender, unusedPaymentToken_);
+        _token_.transfer(msg.sender, unusedToken_);
     }
 
-    function _addLiquidity(uint256 amountPayment_, uint256 amountToken_) internal returns(uint256 lpAmount_)
-    {
-        IERC20 _payment_ = IERC20(addressBook.get("payment"));
-        IToken _token_ = IToken(addressBook.get("token"));
-        IUniswapV2Router02 _router_ = IUniswapV2Router02(addressBook.get("router"));
-        require(address(_payment_) != address(0), "Payment token not set");
+    /**
+     * Sell LP
+     * @param lpAmount_ LP amount that user is going to sell
+     * @return paymentAmount_ USDC amount that user received
+     * @dev approve LP before this function calling
+     */
+    function sellLP(uint256 lpAmount_) public returns (uint256 paymentAmount_) {
+        if (
+            _routerAddress == address(0) ||
+            _tokenAddress == address(0) ||
+            _usdcAddress == address(0) ||
+            _lpAddress == address(0)
+        ) updateAddresses();
+
+        IERC20 _usdc_ = IERC20(_usdcAddress);
+        IERC20 _token_ = IERC20(_tokenAddress);
+        router = IUniswapV2Router02(_routerAddress);
+        IERC20 _lptoken_ = IERC20(_lpAddress);
+        require(address(_usdc_) != address(0), "Payment not set");
         require(address(_token_) != address(0), "Token not set");
-        require(address(_router_) != address(0), "Router not set");
-        if(amountPayment_ <= 0 || amountToken_ <= 0) return 0;
-
-        _payment_.approve(address(_router_), amountPayment_);
-        _token_.approve(address(_router_), amountToken_);
-        (,, lpAmount_) = _router_.addLiquidity(
-            address(_payment_),
-            address(_token_),
-            amountPayment_,
-            amountToken_,
-            0,
-            0,
-            msg.sender,
-            block.timestamp
-        );
-        return lpAmount_;
-    }
-
-    function sellLP(uint256 lpAmount_) public returns(uint256 paymentAmount_)
-    {
-        IUniswapV2Factory _factory_ = IUniswapV2Factory(addressBook.get("factory"));
-        address _lpAddress_ = _factory_.getPair(
-            addressBook.get("payment"),
-            addressBook.get("token")
-        );
-
-        require(IERC20(_lpAddress_).balanceOf(msg.sender) >= lpAmount_, "Invalid amount");
-        IERC20(_lpAddress_).transferFrom(msg.sender, address(this), lpAmount_);
-
-        paymentAmount_ =  _sellLPWithUSDC(lpAmount_);
-        return paymentAmount_;
-    }
-
-    function _sellLPWithUSDC(uint256 lpAmount_) internal returns(uint256 paymentAmount_){
-        IERC20 _payment_ = IERC20(addressBook.get("payment"));
-        require(address(_payment_) != address(0), "Payment not set");
-        IERC20 _token_ = IERC20(addressBook.get("token"));
-        require(address(_token_) != address(0), "Token not set");
-        IUniswapV2Router02 _router_ = IUniswapV2Router02(addressBook.get("router"));
-        require(address(_router_) != address(0), "Router not set");
-        IUniswapV2Factory _factory_ = IUniswapV2Factory(addressBook.get("factory"));
-        address _lpAddress_ = _factory_.getPair(
-            addressBook.get("payment"),
-            addressBook.get("token")
-        );
-        IERC20 _lptoken_ = IERC20(_lpAddress_);
+        require(address(router) != address(0), "Router not set");
         require(address(_lptoken_) != address(0), "_lptoken_ not set");
 
-        if(lpAmount_ <= 0) return 0;
-        _lptoken_.approve(address(_router_), lpAmount_);
+        require(_lptoken_.balanceOf(msg.sender) >= lpAmount_, "Invalid amount");
+        _lptoken_.transferFrom(msg.sender, address(this), lpAmount_);
+
+        if (lpAmount_ <= 0) return 0;
+
+        _lptoken_.approve(address(router), lpAmount_);
         uint256 _tokenBalanceBefore_ = _token_.balanceOf(address(this));
-        (uint256 _USDCFromRemoveLiquidity_,) = _router_.removeLiquidity(
-            address(_payment_),
+        (uint256 _USDCFromRemoveLiquidity_, ) = router.removeLiquidity(
+            address(_usdc_),
             address(_token_),
             lpAmount_,
             0,
             0,
             address(this),
-            block.timestamp
+            block.timestamp + 1
         );
 
-        uint256 _tokenBalance_ = _token_.balanceOf(address(this)) - _tokenBalanceBefore_;
-        if(_tokenBalance_ == 0) return 0;
-        _token_.approve(address(_router_), _tokenBalance_);
+        uint256 _tokenBalance_ = _token_.balanceOf(address(this)) -_tokenBalanceBefore_;
+        if (_tokenBalance_ == 0) return 0;
+
+        _token_.approve(address(router), _tokenBalance_);
         address[] memory path = new address[](2);
         path[0] = address(_token_);
-        path[1] = address(_payment_);
-
-        uint256 _USDCbalanceBefore_ = _payment_.balanceOf(address(this));
-        _router_.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        path[1] = address(_usdc_);
+        uint256 _USDCbalanceBefore_ = _usdc_.balanceOf(address(this));
+        router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
             _tokenBalance_,
             0,
             path,
             address(this),
-            block.timestamp
+            block.timestamp + 1
         );
-        uint256 _USDCFromSwap = _payment_.balanceOf(address(this)) -_USDCbalanceBefore_;
+        uint256 _USDCFromSwap = _usdc_.balanceOf(address(this)) -
+            _USDCbalanceBefore_;
 
-        _payment_.transfer(msg.sender, _USDCFromRemoveLiquidity_ + _USDCFromSwap);
+        _usdc_.transfer(msg.sender, _USDCFromRemoveLiquidity_ + _USDCFromSwap);
 
-        return _USDCFromRemoveLiquidity_ + _USDCFromSwap;
+        paymentAmount_ = _USDCFromRemoveLiquidity_ + _USDCFromSwap;
     }
 
     /**
